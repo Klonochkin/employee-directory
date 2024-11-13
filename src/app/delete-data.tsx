83a79f8bc00f21d26cd5a0b9607482ef5c111351
Klonochkin <di630538@yandex.ru>
2024-11-13 14:18:51 +0300

    impr: Упрощает передачу данных в функцию deleteDate

--- src/app/delete-data.tsx
+++ src/app/delete-data.tsx
@@ -3,6 +3,5 @@
 import { deleteData } from './postgresDb';
 
 export async function deleteSomething(params: FormData) {
-    const id: string = String(params.get('id'));
-    deleteData(Number(id));
+    deleteData(Number(params.get('id')));
 }
