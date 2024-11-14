30c2aae9aae319ca609ac5f987576050e6c62e2d
Klonochkin <di630538@yandex.ru>
2024-11-14 11:12:39 +0300

    feat: Добавляет vercel pg

--- src/app/postgresDb.tsx
+++ src/app/postgresDb.tsx
@@ -3,7 +3,7 @@ import { employee } from '@/app/db/schema';
 import { sql, asc, desc, eq } from 'drizzle-orm';
 import { convertDateToISO } from './convert-date';
 
-const db = drizzle(process.env.DATABASE_URL!);
+const db = drizzle(process.env.POSTGRES_URL!);
 
 export async function getEmployees(name = '', page: number, sort?: string) {
     let offset = (page - 1) * 10;
